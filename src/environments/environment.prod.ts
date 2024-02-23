b6aeaa9e2f9aa69217d7186d1c75f386c134d8fa
skanderKha <skander@atayen.us>
2024-02-23 16:57:23 +0100

    update explorer rpc and chainId , change case in header to avoid alert unsupported network

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -86,10 +86,10 @@ export const environment = {
     currencySymbol:'BTT'
   },
   artheraNetwork: {
-    chainIDHex:'0x2803',
+    chainIDHex:'0x2802',
     chainName: 'Arthera',
     rpcURL:'https://rpc.arthera.net',
-    blockExplorerURL:'https://explorer.arthera.net',
+    blockExplorerURL:'https://explorer.arthera.net/',
     currencySymbol:'AA'
   },
   core : [
